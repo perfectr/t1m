c7249e92632d82ec669dfa31e85b06bcbec70035
Callum McCaul <mccaulcall@myvuw.ac.nz>
2015-01-27 15:41:57 +1300

    Fixed Entity type issues

--- T1MWebApp/src/main/java/nz/govt/doc/t1m/domain/dataSheets/birdCount/BirdCountEntity.java
+++ T1MWebApp/src/main/java/nz/govt/doc/t1m/domain/dataSheets/birdCount/BirdCountEntity.java
@@ -2,11 +2,30 @@ package nz.govt.doc.t1m.domain.dataSheets.birdCount;
 
 import nz.govt.doc.t1m.domain.dataSheets.DataSheetEntity;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
 /**
  * Created by McCaulC on 27/01/2015.
  */
+@Entity
 public class BirdCountEntity extends DataSheetEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer dataSheetId;
+    @NotNull
+    private Integer surveyId;
+
     public BirdCountEntity(Integer surveyId) {
-        super(surveyId);
+        this.surveyId = surveyId;
+    }
+
+    public Integer getDataSheetId() {
+        return dataSheetId;
+    }
+
+    public Integer getSurveyId() {
+        return surveyId;
     }
 }
--- T1MWebApp/src/main/java/nz/govt/doc/t1m/domain/dataSheets/incidentalBird/IncidentalBirdEntity.java
+++ T1MWebApp/src/main/java/nz/govt/doc/t1m/domain/dataSheets/incidentalBird/IncidentalBirdEntity.java
@@ -2,11 +2,33 @@ package nz.govt.doc.t1m.domain.dataSheets.incidentalBird;
 
 import nz.govt.doc.t1m.domain.dataSheets.DataSheetEntity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
 /**
  * Created by McCaulC on 27/01/2015.
  */
+@Entity
 public class IncidentalBirdEntity extends DataSheetEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer dataSheetId;
+    @NotNull
+    private Integer surveyId;
+
     public IncidentalBirdEntity(Integer surveyId) {
-        super(surveyId);
+        this.surveyId = surveyId;
+    }
+
+    public Integer getDataSheetId() {
+        return dataSheetId;
+    }
+
+    public Integer getSurveyId() {
+        return surveyId;
     }
 }
