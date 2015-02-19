7501599bd37908558098491be0fb20e12cbf83fe
Celdamar <marcelblokker1@yahoo.co.nz>
2015-02-19 14:47:09 +1300

    removed platforms files from git repo

--- T1MClient/platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-C:\Users\blokkerm\Projects\T1M\t1m\T1MClient\platforms\android\CordovaLib\ant-build\AndroidManifest.xml : \
-C:\Users\blokkerm\Projects\T1M\t1m\T1MClient\platforms\android\CordovaLib\AndroidManifest.xml \
--- T1MClient/platforms/android/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-c:\Users\blokkerm\Projects\T1M\t1m\T1MClient\platforms\android\ant-build\AndroidManifest.xml : \
-c:\Users\blokkerm\Projects\T1M\t1m\T1MClient\platforms\android\AndroidManifest.xml \
